bea77986b6772f17b8c089ac4a5c7b99de66ef16
tmvnszktmxpf <tmvnszktmxpf@gmail.com>
2022-06-14 00:30:43 +0900

    작가 페이지도 인증구현

--- fs/fileread.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('tes3t.txt', 'utf8', (err, data) => {
-//   if (err) throw err;
-if(err) {console.log('에러 발생!!'); return;}
-  console.log(data);
-  console.log(2534);
-});
-console.log(6784);
