fc6d002f74e515058d42752b61cff52f52c1d7a2
tmvnszktmxpf <tmvnszktmxpf@gmail.com>
2022-06-16 00:59:05 +0900

    CHG: passport.js logout

--- main.js
+++ main.js
@@ -15,6 +15,11 @@ var session = require('express-session')
 var FileStore = require('session-file-store')(session);
 
 
+var authData = {
+  email: 'diable@naver.com',
+  password: 'vkjhzkxchvkasdf',
+  nickname: 'asdfasdf'
+}
 
 
 app.use(express.static('public'));
@@ -34,6 +39,44 @@ app.use(session({
 }))
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  console.log('serializeUser', user);
+  cb(null, user.email);
+});
+
+passport.deserializeUser(function (user, cb) {
+  console.log('desecializeUser', user);
+  cb(null, authData);
+});
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function verify(username, password, cb) {
+    console.log('localstrategy', username, password);
+    if (username === authData.email) {
+      if (password === authData.password) {
+        console.log("LOGIN SUSS");
+        return cb(null, authData);
+      } else {
+        console.log("incorrect paswsord");
+        return cb(null, false, { message: "incorrect password" });
+      }
+    } else {
+      console.log("who are ayo?");
+      return cb(null, false, { message: "who are you?" });
+    };
+  }));
+
+app.post('/login/login_process', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 
 app.use('/page', pageRouter);
@@ -42,6 +85,7 @@ app.use('/login', loginRouter);
 
 
 app.get('/', (req, res) => {
+  console.log(req.user);
   var title = 'Welcome';
   var description = 'Hello, Node.js';
   var list = template.list(req.list);
