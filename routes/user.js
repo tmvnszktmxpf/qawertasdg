7a25e95aba61fb82705930dc3911d938c62e9633
hoppe <hoppe@101.inc>
2022-07-13 20:15:47 +0900

    chg :test

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,18 @@ const user = require('../lib/user');
 
 
 
+const path = require("path");
+
+const fs = require("fs");
+const filePath = path.join(__dirname, "item_ids.csv_output.csv");
+
 
 
 router.post('/', (req, res, next) => {
     console.log('routeuser');
+    const data = fs.readFileSync(filePath,{encoding:"utf8"});
+    const rows = data.split("\n");
+    console.log(data);
     user.user(req, res, next);
 })
 
