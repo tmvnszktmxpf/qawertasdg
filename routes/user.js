53b324e2d5d4e808eef692b7474c8f9de958c704
hoppe <hoppe@101.inc>
2022-07-13 21:13:17 +0900

    fix : route에서 하던 거 lib로 옮김

--- routes/user.js
+++ routes/user.js
@@ -4,28 +4,11 @@ const user = require('../lib/user');
 
 
 
-const path = require("path");
-
-const fs = require("fs");
-const filePath = path.join(__dirname, "item_ids.csv_output.csv");
 
 
 
 router.post('/', (req, res, next) => {
     console.log('routeuser');
-    const data = fs.readFileSync(filePath,{encoding:"utf8"});
-    const rows = data.split("\n");
-    const post = req.body;
-    console.log(post.userid);
-
-    rows.forEach(row=>{
-        const separators = [' ', ',','"','[',']'];
-        let sp = row.split(/["' ,\[\]]/);
-        sp = sp.filter(word=>word.length>2);
-        if (sp[0] === post.userid) {
-            console.log(sp);
-        }
-    });
 
     user.user(req, res, next);
 })
