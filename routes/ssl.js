a6ff275c417c09c8c6b3f21ef29989d63c9b9702
hoppe <hoppe@101.inc>
2022-07-14 15:14:05 +0900

    chg : test trino

---
+++ routes/ssl.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+
+router.get('/', function(req, res, next){
+  res.send("G_VCXXtebJu-wt1okZ_qHcVtGIdWyuwDDwBsYEqEf8I.GAAbslBdHFZ1-ZW2f3EATeAOIKvIlTqX12JaA26-DB4");
+});
+module.exports = router;
