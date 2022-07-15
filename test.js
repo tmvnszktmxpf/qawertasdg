0658a46f6af450ff42867d1139df0c3f53696d8d
hoppe <hoppe@101.inc>
2022-07-15 12:40:46 +0900

    CHG : ec2 trino conn

--- test.js
+++ test.js
@@ -1,85 +1,4 @@
 
-// const presto  = require('presto-client');
-
-// const client = new presto.Client({
-//     user: 'myname',
-//     host: 'trino.datasvc.class101.net',
-//     port: 80,
-//     user: 'hoppe',
-//     catalog:'hive',
-//     schema:'svc_dp'
-// });
-
-// client.execute({
-//   query:   'SELECT count(*) as cnt FROM tblname WHERE ...',
-//   catalog: 'hive',
-// //   schema:  'default',
-//   source:  'nodejs-client',
-//   state:   function(error, query_id, stats){ console.log({message:"status changed", id:query_id, stats:stats}); },
-//   columns: function(error, data){ console.log({resultColumns: data}); },
-//   data:    function(error, data, columns, stats){ console.log(data); },
-//   success: function(error, stats){},
-//   error:   function(error){}
-// });
-
-
-// console.log(client);
-
-
-
-// const lento = require('lento');
-
-// const client = lento({
-//     hostname: 'trino.datasvc.class101.net',
-//     port: 80,
-//     catalog: 'hive',
-//     schema: 'svc_dp',
-//     user: 'airflow',
-//     protocol: 'http:',
-//     headers: 'X-Trino-Transaction-Id',
-//     source: 'nodejs-client',
-//     timezoen: 'UTC'
-//   });
-
-// console.log(client);
-
-
-// client.query(`SELECT * FROM svc_dp.products_meta LIMIT 5`,(err,data)=>{
-//     console.log(err);
-//     console.log(data);
-// })
-
-
-
-
-// var presto = require('presto-client');
-// var client = new presto.Client({
-//     host: 'trino.datasvc.class101.net',
-//     port: 80,
-//     catalog: 'hive',
-//     schema: 'svc_dp',
-//     user: 'airflow',
-//     protocol: 'http:',
-//     headers: 'X-Trino-Transaction-Id',
-//     source: 'nodejs-client',
-//     timezoen: 'UTC'
-// });
-
-
-// client.execute({
-//     query:   'SELECT * FROM svc_dp.products_meta LIMIT 5',
-//     catalog: 'hive',
-//     schema:  'svc_dp',
-//     source:  'nodejs-client',
-//     state:   function(error, query_id, stats){ console.log({message:"status changed", id:query_id, stats:stats}); },
-//     columns: function(error, data){ console.log({resultColumns: data}); },
-//     data:    function(error, data, columns, stats){ console.log(data); },
-//     success: function(error, stats){},
-//     error:   function(error){}
-// })
-
-// console.log(client.execute());
-
 var trino = require('@dalongrong/trino-client');
 var client = new trino.Client({
     host: 'trino.datasvc.class101.net',
@@ -87,20 +6,14 @@ var client = new trino.Client({
     catalog: 'hive',
     schema: 'svc_dp',
     user: 'airflow',
-    protocol: 'http:',
-    headers: 'X-Trino-Transaction-Id',
-    source: 'nodejs-client',
-    timezoen: 'UTC'
 });
 
 client.execute({
-  query:   'SELECT * FROM svc_dp.products_meta LIMIT 5',
-  catalog: 'hive',
-  schema:  'default',
-  source:  'nodejs-client',
-  state:   function(error, query_id, stats){ console.log({message:"status changed", id:query_id, stats:stats}); },
-  columns: function(error, data){ console.log({resultColumns: data}); },
-  data:    function(error, data, columns, stats){ console.log(data); },
+  query:   'SELECT * FROM svc_dp.products_meta LIMIT 1',
+  data:    function(error, data, columns, stats){ 
+    console.log(data[0][10]);
+    // console.log(columns);
+    },
   success: function(error, stats){},
   error:   function(error){}
 });
